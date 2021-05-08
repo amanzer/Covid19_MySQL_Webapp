c895d9bde497759167609cc352648b1985ae4c52
amanzer <44897538+amanzer@users.noreply.github.com>
2021-05-08 14:22:26 +0200

    Utilisateurs devs : épidémiologist : id: 0e61cae8-546f-404f-a33a-7069127118c5 password : testing utilisateur normal : id:usdd password : usdd
    - un bug à corriger : si iso_code pas valide ça lance une erreur de fk
    - refaire la vue html et css

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+"""
+Utilisateurs devs :
+épidémiologist : id: 0e61cae8-546f-404f-a33a-7069127118c5 password : testing
+utilisateur normal : id:usdd password : usdd
+
+
+"""
+
 from flask import Flask, render_template, url_for, redirect, request, session
 import os
 import string
@@ -6,6 +14,7 @@ from flask_mysql_connector import MySQL
 from sql_commands import project_requests, database_commands
 from datetime import timedelta
 
+# Paramètres de Flask
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.permanent_session_lifetime = timedelta(minutes=30)
@@ -14,34 +23,16 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DATABASE'] = 'coviddata'
 app.config['DEBUG'] = True
 
-mysql = MySQL(app)
-epidemiologist = False
-
-
-# class User:
-#     def __init__(self, id=None, first_name=None, last_name=None, username=None, address=None, password=None):
-#         self.id = id
-#         self.username = username
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.username = username
-#         self.address = address
-#         self.password = password
-#
-#     def __repr__(self):
-#         return f'<User>'
-#
-#     def logout(self):
-#         self.id=None
-#         self.username = None
-#         self.first_name = None
-#         self.last_name = None
-#         self.username = None
-#         self.address = None
-#         self.password = None
+# Les variables globales
+mysql = MySQL(app)  # la BDD
+epidemiologist = False  # pour choisir l'interface à afficher
+thisUserId = ""  # Stock l'ID de l'utilisateur connecté
 
 
 def executeMySqlCommand(query):
+    """
+    exécute la query sql
+    """
     cur = mysql.connection.cursor()
     cur.execute(query)
     fetchedData = cur.fetchall()
@@ -50,6 +41,10 @@ def executeMySqlCommand(query):
 
 
 def generate_user_id():
+    """
+    Génère aléatoirement un ID
+    J'ai enlevé la vérification car les chances de tomber sur une clé qui existe déjà sont vraiment minime
+    """
     p1 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
     p2 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
     p3 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
@@ -57,20 +52,16 @@ def generate_user_id():
     p5 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
 
     newId = p1 + "-" + p2 + "-" + p3 + "-" + p4 + "-" + p5
-    # while True:
-    #     isIdValidCommand = ("SELECT EXISTS (SELECT * FROM person WHERE person.id = '%s');" % (newId))
-    #     res = executeMySqlCommand(isIdValidCommand)
-    #     if res[0][0] == 0:
-    #         break
-    #     else:
-    #         newId=''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
     return newId
 
 
 def createUser(username, first_name, last_name, address, password):
+    """
+    Créer un nouvel utilisateur dans la bdd et renvoie le nouvel user ID crée.
+    """
     newId = generate_user_id()
     insertCommand = "INSERT INTO person(id, first_name, last_name, username, address, password) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (
-    newId, first_name, last_name, username, address, password)
+        newId, first_name, last_name, username, address, password)
     cur = mysql.connection.cursor()
     cur.execute(insertCommand)
     cur.execute("COMMIT;")
@@ -80,8 +71,11 @@ def createUser(username, first_name, last_name, address, password):
 
 
 def accountExists(given_userID, given_password):
+    """
+    Vérifie si le compte existe dans la bdd.
+    """
     isUserValidCommand = ("SELECT EXISTS (SELECT * FROM person WHERE person.id = '%s' AND person.password= '%s');" % (
-    given_userID, given_password))
+        given_userID, given_password))
 
     res = executeMySqlCommand(isUserValidCommand)
     if res[0][0] == 1:
@@ -90,8 +84,46 @@ def accountExists(given_userID, given_password):
         return False
 
 
-def isUserEpidemiologist():
-    return True
+def isUserEpidemiologist(userId):
+    """
+    Vérifie si le compte qui se connecte est un épidemiologiste
+    """
+    epidemiologistIdCommand=("SELECT EXISTS (SELECT * FROM epidemiologist WHERE epidemiologist.id_person = '%s');" % userId)
+    res = executeMySqlCommand(epidemiologistIdCommand)
+    if res[0][0] == 1:
+        return True
+    else:
+        return False
+
+
+def addNewDataToHospitals(iso_code, date, icu_patients, hosp_patients):
+    """
+    Ajoute les données dans la table hospitalsdata
+
+    TODO: Si iso_code n'éxiste pas lance une erreur de fk
+    """
+    userId = thisUserId
+    insertCommand = "INSERT INTO hospitalsdata (iso_code, date, icu_patients, hosp_patients, source_epidemiologist) VALUES ('%s', '%s', '%s', '%s', '%s');" % (
+        iso_code, date, icu_patients, hosp_patients, userId)
+    cur = mysql.connection.cursor()
+    cur.execute(insertCommand)
+    cur.execute("COMMIT;")
+    cur.close()
+    print(insertCommand)
+
+
+########################################################################################################################
+"""
+Les méthodes qui gèrent les pages de l'appli web:
+- /login et /
+- /logout
+- /register
+- /lostPassword
+- /homePage
+- /showData
+- /showRequest
+- /modifyData
+"""
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -99,12 +131,13 @@ def isUserEpidemiologist():
 def login():
     if request.method == "POST":
         session.pop('user', None)
-
         given_userID = request.form['userID']
         given_password = request.form['password']
+
         if accountExists(given_userID, given_password) and given_password != "" and given_userID != "":
-            global epidemiologist
-            epidemiologist = isUserEpidemiologist()
+            global epidemiologist, thisUserId
+            epidemiologist = isUserEpidemiologist(given_userID)
+            thisUserId = given_userID
             session['user'] = request.form['userID']
             # session.permanent =True
             return redirect(url_for('home'))
@@ -119,7 +152,9 @@ def login():
 
 @app.route('/logout')
 def logout():
+    global thisUserId
     session.pop('user', None)
+    thisUserId = ""
     return render_template('login.html')
 
 
@@ -189,7 +224,7 @@ def modifyData():
             icu_patients = request.form["icu_patients"]
             hosp_patients = request.form["hosp_patients"]
             if iso_code != "" and date != "" and icu_patients != "" and hosp_patients:
-                print(iso_code + " " + date + " " + icu_patients + " " + hosp_patients)
+                addNewDataToHospitals(iso_code, date, icu_patients, hosp_patients)
                 return render_template("homePage.html", userEpi=epidemiologist)
             else:
                 return render_template("modifyHospitalsData.html", userEpi=epidemiologist,
@@ -201,5 +236,4 @@ def modifyData():
 
 
 if __name__ == '__main__':
-    # thisUser= User()
     app.run()
